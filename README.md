a293529f7dff6c18971b1530fb7a911b08ab03a9
MrSyabro <syabrocraft@gmail.com>
2021-07-18 16:20:48 +0300

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Telegram sound bot
+
+Телеграм бот, который воспроизводит музыку на ESP32
+
+## Usage
+
+TODO
---
+++ main/main.h
@@ -0,0 +1,44 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+#include "sdkconfig.h"
+#include "audio_element.h"
+#include "audio_pipeline.h"
+#include "audio_event_iface.h"
+#include "audio_common.h"
+#include "http_stream.h"
+#include "i2s_stream.h"
+#include "mp3_decoder.h"
+
+#include "esp_peripherals.h"
+#include "periph_wifi.h"
+#include "board.h"
+
+#if __has_include("esp_idf_version.h")
+#include "esp_idf_version.h"
+#else
+#define ESP_IDF_VERSION_VAL(major, minor, patch) 1
+#endif
+
+#if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 1, 0))
+#include "esp_netif.h"
+#else
+#include "tcpip_adapter.h"
+#endif
+
+
+#ifndef MAIN_H
+#define MAIN_H
+static const char *tgbt = CONFIG_TGBT;
+audio_pipeline_handle_t pipeline;
+audio_element_handle_t http_stream_reader, i2s_stream_writer, mp3_decoder;
+
+void set_url(char *url);
+
+QueueHandle_t url_queue;
+#endif
+
